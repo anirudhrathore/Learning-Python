58112456bc16076fc7277fb44d696d57bb6a521a
Aryan Dadheech <42897814+n0obs73r@users.noreply.github.com>
2019-08-05 16:51:07 +0530

    Update exponent_function.py

--- exponent_function.py
+++ exponent_function.py
@@ -6,3 +6,12 @@ def raise_to_power(base_num, pow_num):
     return result
 
 print(raise_to_power(2, 3)) 
+
+
+''' 
+another way to do eet
+'''
+a = float(input('Enter first digit: '))
+b = float(input('Enter second digit: '))
+result= a**b
+print(result)
